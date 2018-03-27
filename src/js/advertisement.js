28603758798458d077373c8c6c92ad3c1f05dd53
Julia Peris Ferrer <JULI@MacBook-Pro-de-Julia-2.local>
2018-03-27 19:31:31 +0200

    carrousel y menu

--- src/js/advertisement.js
+++ src/js/advertisement.js
@@ -3,3 +3,7 @@ import $ from './jquery-3.3.1.min';
 $(".close").on("click", function(){
     $(".advertisement").remove();
 });
+
+$(".advertisement").on("click", function(){
+    $(this).remove();
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 import $ from './jquery-3.3.1.min';
 import './advertisement';
 import './article';
+import './carousel';
+import './menu';
